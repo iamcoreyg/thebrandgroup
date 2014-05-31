fd6e05965f3d298813fd1a43100c84a1c84cc30b
iamcoreyg <iamcoreyg@gmail.com>
2014-05-31 15:29:26 -0700

    whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -365,7 +365,6 @@ module.exports = function (grunt) {
         }
     });
 
-
     grunt.registerTask('serve', function (target) {
         if (target === 'dist') {
             return grunt.task.run(['build', 'connect:dist:keepalive']);
